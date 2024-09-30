c51b0aab5457af1bc590644d6e9497dcbbc5ca1d
blue129 <15110234020@qq.com>
2024-09-30 20:37:42 +0800

    初始化样式

--- DECO7140/assessment/js/index.js
+++ DECO7140/assessment/js/index.js
@@ -1,7 +1,17 @@
 // Imports
 // Variables & Constants
+
 // Event Listeners
+document.addEventListener("DOMContentLoaded", function() {
+    logConsoleMessage();
+});
+
 // Startup code
 // Functions - specific behaviours
+// Console Function
+function logConsoleMessage() {
+    console.log("JavaScript The JavaScript file has been loaded.");
+}
 
 // Functions - genral
+
